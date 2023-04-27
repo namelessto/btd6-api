d9867a89e3397912172decef4d1bb4cdf56f2a32
Shai <shaitnto@gmail.com>
2023-04-27 11:22:17 +0300

    removed todo

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.get("/towers", (req, res) => {
   const folderPath = "./BTD6";
 
   const files = fs.readdirSync(folderPath);
-  let jsonFiles = [];
   for (const file of files) {
     // Load the JSON file into a JavaScript variable.
     const jsonData = JSON.parse(fs.readFileSync(folderPath + "/" + file));
@@ -26,45 +25,13 @@ app.get("/towers", (req, res) => {
 });
 
 // GET a single todo by id
-app.get("/towers/:id", (req, res) => {
+app.get("/tower/:id", (req, res) => {
   const fileID = req.params.id;
   const filePath = `./BTD6/${fileID}.json`;
 
   const jsonData = JSON.parse(fs.readFileSync(filePath));
 
   res.json(jsonData);
-});
-
-// POST a new todo
-app.post("/todos", (req, res) => {
-  const todo = req.body;
-  todo.id = todos.length + 1;
-  todos.push(todo);
-  res.status(201).json(todo);
-});
-
-// PUT (update) a todo by id
-app.put("/todos/:id", (req, res) => {
-  const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
-  if (!todo) {
-    res.status(404).json({ message: "Todo not found" });
-  } else {
-    Object.assign(todo, req.body);
-    res.json(todo);
-  }
-});
-
-// DELETE a todo by id
-app.delete("/todos/:id", (req, res) => {
-  const todoIndex = todos.findIndex(
-    (todo) => todo.id === parseInt(req.params.id)
-  );
-  if (todoIndex === -1) {
-    res.status(404).json({ message: "Todo not found" });
-  } else {
-    todos.splice(todoIndex, 1);
-    res.status(204).send();
-  }
 });
 
 app.listen(port, () => {
