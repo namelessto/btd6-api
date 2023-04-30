9f08aec680b97649a4c05de5e0c0e518c30e3eba
Shai <shaitnto@gmail.com>
2023-04-30 12:37:50 +0300

    added all the necessary gets

--- app.js
+++ app.js
@@ -27,6 +27,86 @@ app.get("/tower/:id", (req, res) => {
   res.json(jsonData);
 });
 
+app.get("/heroes", (req, res) => {
+  let jsonResponse = [];
+  const { allData } = req.query;
+  const shouldReturnAllData = allData === "true";
+  const folderPath = "./BTD6/Heroes";
+  const fileCategory = "heroes";
+  jsonResponse = handleAllItems(shouldReturnAllData, folderPath, fileCategory);
+
+  res.json(jsonResponse);
+});
+
+app.get("/hero/:id", (req, res) => {
+  const fileID = req.params.id;
+  const filePath = `./BTD6/Heroes/${fileID}.json`;
+
+  const jsonData = JSON.parse(fs.readFileSync(filePath));
+
+  res.json(jsonData);
+});
+
+app.get("/maps", (req, res) => {
+  let jsonResponse = [];
+  const { allData } = req.query;
+  const shouldReturnAllData = allData === "true";
+  const folderPath = "./BTD6/Maps";
+  const fileCategory = "maps";
+  jsonResponse = handleAllItems(shouldReturnAllData, folderPath, fileCategory);
+
+  res.json(jsonResponse);
+});
+
+app.get("/map/:id", (req, res) => {
+  const fileID = req.params.id;
+  const filePath = `./BTD6/Maps/${fileID}.json`;
+
+  const jsonData = JSON.parse(fs.readFileSync(filePath));
+
+  res.json(jsonData);
+});
+
+app.get("/bloons", (req, res) => {
+  let jsonResponse = [];
+  const { allData } = req.query;
+  const shouldReturnAllData = allData === "true";
+  const folderPath = "./BTD6/Bloons";
+  const fileCategory = "bloons";
+  jsonResponse = handleAllItems(shouldReturnAllData, folderPath, fileCategory);
+
+  res.json(jsonResponse);
+});
+
+app.get("/bloon/:id", (req, res) => {
+  const fileID = req.params.id;
+  const filePath = `./BTD6/Bloons/${fileID}.json`;
+
+  const jsonData = JSON.parse(fs.readFileSync(filePath));
+
+  res.json(jsonData);
+});
+
+app.get("/bosses", (req, res) => {
+  let jsonResponse = [];
+  const { allData } = req.query;
+  const shouldReturnAllData = allData === "true";
+  const folderPath = "./BTD6/Bosses";
+  const fileCategory = "bosses";
+  jsonResponse = handleAllItems(shouldReturnAllData, folderPath, fileCategory);
+
+  res.json(jsonResponse);
+});
+
+app.get("/boss/:id", (req, res) => {
+  const fileID = req.params.id;
+  const filePath = `./BTD6/Bosses/${fileID}.json`;
+
+  const jsonData = JSON.parse(fs.readFileSync(filePath));
+
+  res.json(jsonData);
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
