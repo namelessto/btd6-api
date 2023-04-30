daee921625e9082987ded57cef477301fd167dac
Shai <shaitnto@gmail.com>
2023-04-30 12:58:13 +0300

    removed unnecessary package

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/towers", (req, res) => {
   let jsonResponse = [];
